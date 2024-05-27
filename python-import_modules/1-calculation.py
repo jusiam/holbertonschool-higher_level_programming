eba778f01259a257682267d18732e61399fa8949
jusiam <mossakowska.justyna@gmail.com>
2024-05-27 12:42:02 +0300

    1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,3 +5,9 @@ if __name__ == "__main__":
     b = 5
     c = add(a, b)
     print("{} + {} = {}".format(a, b, c))
+    c = sub(a, b)
+    print("{} - {} = {}".format(a, b, c))
+    c = mul(a, b)
+    print("{} * {} = {}".format(a, b, c))
+    c = div(a, b)
+    print("{} / {} = {}".format(a, b, c))
