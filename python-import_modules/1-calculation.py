656cfd1c4f4ce28fa89d7c1dab1c05c00d3ba93e
jusiam <mossakowska.justyna@gmail.com>
2024-05-27 12:38:05 +0300

    1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import calculator_1
     a = 10
     b = 5
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
