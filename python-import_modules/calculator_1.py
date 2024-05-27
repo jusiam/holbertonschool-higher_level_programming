db29798233101aecdfd97291eaab2dadb3a15592
jusiam <mossakowska.justyna@gmail.com>
2024-05-27 15:32:24 +0300

    1

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,51 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
+    add = cal.add(a, b)
+    sub = cal.sub(a, b)
+    mul = cal.mul(a, b)
+    div = cal.div(a, b)
