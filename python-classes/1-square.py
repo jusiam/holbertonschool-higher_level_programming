bda4d2b5f498bcf5e11e6c80d55e8a8ac161883b
jusiam <mossakowska.justyna@gmail.com>
2024-06-19 16:56:26 +0300

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square():
+    "def private size attribute"
+    def __init__(self, size):
+        "initalize square using pirvate attribute size as args"
+        self.__size = size
+
+    pass
