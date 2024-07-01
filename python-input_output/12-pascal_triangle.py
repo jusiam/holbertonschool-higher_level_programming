dacfe480d88779f9cd31625f5a19340cff76bfe5
jusiam <mossakowska.justyna@gmail.com>
2024-07-01 13:38:14 +0300

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,7 +5,23 @@ This script adds all arguments to a Python list,
 and then saves them to a file.
 """
 
-def pascal_triangle(n)
+def pascal_triangle(n):
+height = int(input("Enter height of triangle:"))
+t_List = []
+for R in t_List(0,height+1):
+    n_List = []
+    if R == 0:
+        n_List = [1]
+    elif R == 1:
+        n_List == [1,1]
+    else:
+        for C in R:
+            if C == 0:
+                n_List.append(1)
+            elif C == R:
+                n_List.append(1)
+            else:
+                A = t_List()
 
 if n <= 0:
     return []
