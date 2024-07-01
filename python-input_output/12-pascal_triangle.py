2babc1e6f2aef39c165eeb2da0972c07df0c9c11
jusiam <mossakowska.justyna@gmail.com>
2024-07-01 13:39:35 +0300

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,22 +6,15 @@ and then saves them to a file.
 """
 
 def pascal_triangle(n):
-height = int(input("Enter height of triangle:"))
-t_List = []
-for R in t_List(0,height+1):
-    n_List = []
-    if R == 0:
-        n_List = [1]
-    elif R == 1:
-        n_List == [1,1]
-    else:
-        for C in R:
-            if C == 0:
-                n_List.append(1)
-            elif C == R:
-                n_List.append(1)
-            else:
-                A = t_List()
+  height = int(input("Enter height of triangle: "))
+  triangle = []
+  row = []
+  prev_row = []
+  for i in xrange(0, height + 1):
+    row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] + prev_row[j] or 1 for j in xrange(0, i)]
+    prev_row = row
+    triangle += [row]
+  return triangle[1:]
 
 if n <= 0:
     return []
