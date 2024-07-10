73b8b6af4a553382600605f520730c267e998063
jusiam <mossakowska.justyna@gmail.com>
2024-07-10 12:45:18 +0300

    8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,9 @@
 -- Query to list all cities of California without using JOIN
 SELECT id, name
 FROM cities
-WHERE name = 'California'
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
 ORDER BY id ASC;
