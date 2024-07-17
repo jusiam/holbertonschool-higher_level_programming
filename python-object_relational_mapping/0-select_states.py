32999f9d8932d2db8f6d49e2e4a24b1f58112d77
jusiam <mossakowska.justyna@gmail.com>
2024-07-17 12:31:02 +0300

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-"""
-Script to list all states from the database hbtn_0e_0_usa.
-"""
-
+"""Module that prints all states by ASCENDING order"""
 import MySQLdb
 import sys
 
-
-def list_states(username, password, database):
-    """
-    Connects to the MySQL server and lists all states from
-    the specified database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-    """
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        print("Usage: ./0-select_states.py <username> <password> <database>")
+    """
+    Get MySQL username, password, and database
+    name from command line arguments
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=mysql_username,
+                        password=mysql_password,
+                        database=database_name
+                        )
+
+    c = db.cursor()
+    c.execute("""SELECT * from states ORDER BY id ASC""")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
