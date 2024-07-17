52ddb3b8e70c51e806514cd04ea0d3e67868d4b7
jusiam <mossakowska.justyna@gmail.com>
2024-07-17 12:33:20 +0300

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Module that prints all states by ASCENDING order"""
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
 import MySQLdb
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    """
-    Get MySQL username, password, and database
-    name from command line arguments
-    """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
     db = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=mysql_username,
-                        password=mysql_password,
-                        database=database_name
-                        )
-
-    c = db.cursor()
-    c.execute("""SELECT * from states ORDER BY id ASC""")
-
-    rows = c.fetchall()
-
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    c.close()
+    cur.close()
     db.close()
